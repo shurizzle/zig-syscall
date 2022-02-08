30e509c0c01c46e36f7d5b6d62197377b7ed7f41
shurizzle <shura1991@gmail.com>
2022-02-08 05:30:37 +0100

    Generate apple calls generator

--- macos-generator.rb
+++ macos-generator.rb
@@ -7,6 +7,8 @@ require 'fileutils'
 URL = 'https://opensource.apple.com/source/xnu/xnu-7195.141.2/bsd/kern/syscalls.master'
 TRAPS_URL = 'https://opensource.apple.com/source/xnu/xnu-4570.41.2/osfmk/kern/syscall_sw.c'
 
+puts "Generating consts..."
+
 content = "// BSD's syscalls\n"
 URI.open(URL) do |f|
   f.each_line do |line|
@@ -42,3 +44,42 @@ FileUtils.mkdir_p('src/apple')
 File.open('src/apple/consts.zig', 'wb') do |f|
   f.print content
 end
+
+def generate_call(argno, inst, nr, ret, arg1, arg2, arg3, arg4, arg5, arg6, clobber_regs)
+  content = "pub inline fn syscall#{argno}(n: usize"
+  (1..argno).each do |i|
+    content << ", arg#{i}: usize"
+  end
+  content << ") usize {\n"
+  content << "    return asm volatile (\"#{inst}\"\n"
+  content << "        : [ret] \"={#{ret}}\" (-> usize),\n"
+  content << "        : [n] \"{#{nr}}\" (n),\n"
+  content << "          [arg1] \"{#{arg1}}\" (arg1),\n" if argno > 0
+  content << "          [arg2] \"{#{arg2}}\" (arg2),\n" if argno > 1
+  content << "          [arg3] \"{#{arg3}}\" (arg3),\n" if argno > 2
+  content << "          [arg4] \"{#{arg4}}\" (arg4),\n" if argno > 3
+  content << "          [arg5] \"{#{arg5}}\" (arg5),\n" if argno > 4
+  content << "          [arg6] \"{#{arg6}}\" (arg6),\n" if argno > 5
+  content << "        : #{clobber_regs}\n"
+  content << "    );\n"
+  content << "}\n"
+
+  content
+end
+
+def generate_calls(arch, inst, nr, ret, arg1, arg2, arg3, arg4, arg5, arg6, clobber_regs)
+  puts "Generating #{arch} calls..."
+
+  content = "// NR: %#{nr} return: %#{ret} parameters: %#{arg1}, %#{arg2}, %#{arg3}, %#{arg4}, %#{arg5}, %#{arg6}\n"
+  (0..6).each do |i|
+    content << "\n"
+    content << generate_call(i, inst, nr, ret, arg1, arg2, arg3, arg4, arg5, arg6, clobber_regs)
+  end
+
+  File.open("src/apple/#{arch}/calls.zig", 'wb') do |f|
+    f.print content
+  end
+end
+
+generate_calls 'x86_64', 'syscall', 'rax', 'rax', 'rdi', 'rsi', 'rdx', 'r10', 'r8', 'r9', '"rcx", "r11", "memory"'
+generate_calls 'aarch64', 'svc $$0x80', 'x8', 'x0', 'x0', 'x1', 'x2', 'x3', 'x4', 'x5', '"memory", "cc"'
